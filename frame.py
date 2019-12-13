1cff295f06bfe0f94a2834ea8f5d982731be46c5
Eranziel <eranziel@gmail.com>
2019-12-12 23:08:24 -0600

    Reads input file and outputs to console or another file

---
+++ frame.py
@@ -0,0 +1,17 @@
+
+# Typescript definition for Fame
+# class Frame extends LicensedItem {
+#   private _mechtype: MechType[]
+#   private _y_pos: number
+#   private _mounts: MountType[]
+#   private _stats: IFrameStats
+#   private _traits: FrameTrait[]
+#   private _core_system: CoreSystem
+class Frame:
+    """Class for frame data"""
+    def __init__(self):
+        self.mechtype = []
+        self.mounts = []
+        self.stats = None
+        self.traits = []
+        self.core_system = None
